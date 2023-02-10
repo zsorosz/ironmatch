b2f71f51d334b13c2b2b8393c045d4c0aa72f0bb
zsorosz <oroszi.zsofi@gmail.com>
2023-02-10 15:33:52 +0100

    Set up profile ejs, display student data

--- routes/teacher.routes.js
+++ routes/teacher.routes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const { isLoggedIn } = require("../middleware/route-guard");
+const Teacher = require("../models/Teacher.model");
+const Student = require("../models/Student.model");
 
 /* GET  profile page */
-router.get("/profile", (req, res, next) => {
-  res.render("teacher-views/profile", { user: req.session.user });
+router.get("/profile", async (req, res, next) => {
+  const allStudents = await Student.find();
+  res.render("teacher-views/profile", { user: req.session.user, allStudents });
 });
 
 module.exports = router;
